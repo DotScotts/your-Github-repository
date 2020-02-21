13a4366cb58486841ad246fd4feb561c3703f59e
sivagamasheridan <sivagama.srinivasan@sheridanc.on.ca>
2020-02-21 11:59:50 -0500

    midterm exam

---
+++ src/status/Status.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+import java.util.Scanner;
+
+/** This class takes String input from user 
+ * and calls method statusDetail to print the 
+ * status of User-Rejected,Pending,Processing and Approved.
+ * if the input is not given in String, 
+ * program will accept and produce wrong results. 
+ * Use enumerators to avoid input errors
+ * and print the status details. 
+ * @author srinivsi
+ */
+public class Status {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+    Scanner in =new Scanner(System.in);
+    System.out.println("Enter the user status code (zero,one,two,three) in string");
+    String code = in.next();
+    StausUser t= new StausUser();
+    t.statusDetail(code); 
+    }
+    
+}
---
+++ src/status/StausUser.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package status;
+
+/**
+ *
+ * @author srinivsi
+ */
+public class StausUser 
+{
+   public void statusDetail(String code)
+{
+switch(code.toUpperCase())
+        {
+        case "ZERO": System.out.println("REJECTED");
+        break;
+        case "ONE": System.out.println("PENDING");
+        break;
+        case "TWO":
+        System.out.println("PROCESSING");
+        break;
+        case "THREE": 
+            System.out.println("APPROVED");
+        break;
+        default:
+         System.out.println("NOT VALID CODE");
+        break;
+        }
+}
+ 
+}
